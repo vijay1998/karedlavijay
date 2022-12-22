71d2cdceab6d1fad47ae28ba2857c14881750822
root <root@ip-172-31-82-140.ec2.internal>
2022-12-22 16:35:06 +0000

    first git assignment

---
+++ sample.java
@@ -0,0 +1,11 @@
+public class Main {
+
+ public static void main(string[] args) {
+	  int first = 20;
+	  int second=30;
+	   // add two numbers
+	    int sum = first +second;
+	    System.out.println(firdt +"+"+secound +"=" +sum);
+ }
+
+}
